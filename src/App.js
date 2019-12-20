1603a40f56eed12a3d41d7b34e0714547bd9c471
Christian Fares <christian_fares.23@hotmail.com>
2019-12-20 19:17:33 +1100

    Started working on stucturing the app. Implementing a todo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 
 function App() {
+  
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Button variant="contained" color="primary">
+        This is a material ui Button
+      </Button>
     </div>
   );
 }
---
+++ src/components/Todo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Container from '@material-ui/core/Container'
+
+function Todo() {
+	const [state, setstate] = useState({
+		title: "",
+		description: "",
+		isComplete: false
+	})
+	return (
+		<Container>
+			
+		</Container>
+	)
+}
+
+export default Todo
