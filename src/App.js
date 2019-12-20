6fa0b1d2eeee481ba6d7c0d798c3929c69b9cd9d
Christian Fares <christian_fares.23@hotmail.com>
2019-12-20 23:51:30 +1100

    Added a header to the page, defined a todo element displays as an expansion panel element and displayed a list of todos. Todos can also be marked/unmarked as complete.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React, {useState} from 'react';
+
+import Todo from './components/Todo'
+
+import { Container } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography'
+import AppBar from '@material-ui/core/AppBar';
+import Fab from '@material-ui/core/Fab'
+import AddSharpIcon from '@material-ui/icons/AddSharp';
 
 function App() {
-  
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <Button variant="contained" color="primary">
-        This is a material ui Button
-      </Button>
-    </div>
-  );
+	const [todos, setTodos] = useState([<Todo/>, <Todo/>, <Todo/>])
+
+	return (
+		<React.Fragment>
+			<AppBar style={{padding:"10px"}}>
+				<Typography style={{alignSelf:"center"}} variant="h4">Todo Application</Typography>
+			</AppBar>
+			<Container style = {{paddingTop:"70px"}}>
+				{todos}
+				<Fab style={{margin: "10px", float:"right"}}>
+					<AddSharpIcon />
+				</Fab>
+			</Container>
+		</React.Fragment>
+	);
 }
 
 export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,36 @@
-import React from 'react'
-import Container from '@material-ui/core/Container'
+import React, {useState} from 'react'
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 function Todo() {
-	const [state, setstate] = useState({
-		title: "",
-		description: "",
-		isComplete: false
-	})
+	const [summary, setSummary] = useState("Summary");
+	const [description, setDescription] = useState("");
+	const [isComplete, setIsComplete] = useState(false);
+
+	function handleChange(e) {
+		setIsComplete(e.target.checked)
+	}
+
 	return (
-		<Container>
-			
-		</Container>
+		<ExpansionPanel disabled={isComplete}>
+			{/* the pointer event style is to enable the check box while the expansion panel is disabled */}
+			<ExpansionPanelSummary style={{pointerEvents:"auto"}}>
+				<FormControlLabel
+					control = {<Checkbox />}
+					label={summary}
+					onClick={event => event.stopPropagation()}
+					onChange={handleChange}
+					disabled={false}
+				/>
+			</ExpansionPanelSummary>
+			<ExpansionPanelDetails>
+				{description}
+			</ExpansionPanelDetails>
+		</ExpansionPanel>
 	)
 }
 
